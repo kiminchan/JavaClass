7ff8acefd2db842174095d3b18fcd77f807aea2b
inchan <kiminchan12341@gmail.com>
2022-01-29 14:57:02 +0900

    add List.java , plus Array.java

--- Array.java
+++ Array.java
@@ -21,6 +21,7 @@ public class Array {
     */
     
     // compare(array1, array2) : 배열안의 인자의 갯수에따라 a>b : 1, a<b : -1, else : 0
+    /* 
         int intArr[] = {10, 20, 17, 22, 35};
 
         int intArr1[] = {10, 15, 22};
@@ -28,6 +29,60 @@ public class Array {
         System.out.println("Integer Arrays on comparison: " + Arrays.compare(intArr, intArr1));
 
         System.out.println("Integer Arrays on comparison: " + Arrays.compareUnsigned(intArr, intArr1));
+    */
+    // 배열 반복문 이용방법 
+    /*
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(cars.length);
+
+        for(int i=0; i<cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+
+        for(String i : cars) {
+            System.out.println(i);
+        }
+    */
+    // copyOf() : 전달받은 배열의 특정 길이만큼 새로운 배열 복사
+    /* 
+        int[] arr1 = {1,2,3,4,5};
+        int[] arr2 = Arrays.copyOf(arr1,5);
+
+        for(int i=0; i < arr2.length; i++) {
+            System.out.println(arr2[i] + " ");
+        }
+
+        int[] arr3 = Arrays.copyOf(arr1, 10);
+        for(int i = 0; i< arr3.length; i++) {
+            System.out.print(arr3[i] + " ");
+        }
+    */
+    // copyOfRange() : 전달받은 배열의 특정범위에 해당하는 요소만을 새로운 배열로 복사 
+    /*
+        int[] arr1 = {1,2,3,4,5};
+
+        int[] arr2 = Arrays.copyOfRange(arr1,0,10);
+        for(int i = 0; i<arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+    */
+    // fill() : 전달받은 배열의 모든 요소를 특정값으로 초기화 
+    /*
+        int[] arr = new int[10];
+
+        Arrays.fill(arr, 7);
+
+        for(int i=0; i<arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    */
+    // sort() : 배열의 모든요소를 오름차순으로 정렬 
+        int[] arr = {6,7,8,9,4};
+
+        Arrays.sort(arr);
+        for(int i=0; i<arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
 
     }
 }
