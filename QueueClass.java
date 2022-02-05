28e453f0817e4cbd241587b40925a6f62b094b48
inchan <kiminchan12341@gmail.com>
2022-02-05 19:57:00 +0900

    Add Queue

--- QueueClass.java
+++ QueueClass.java
@@ -1,5 +1,24 @@
 public class QueueClass {
+    public static final int QUEUESIZE = 10;
+    // 데이터 삭제 포인터 
+    private int front = -1;
+    // 데이터 삽입 포인터 
+    private int rear = -1;
+
+    private int[] queue;
+    public QueueClass() {
+        queue = new int[QUEUESIZE];
+    }
+
+    // 큐가 비었는지 확인 
+
+    // 큐가 꽉차있는지 확인 
+
+    // 큐 삽입 
+
+    // 큐 삭제 
+
     public static void main(String[] args) {
-        
+
     }
 }
